61e55c56dddbe47a500d49d5936d57476053b631
mukesh_harshwal@yahoo.co.in <mukesh_harshwal@yahoo.co.in>
2023-07-08 18:54:08 +0530

    added string and simple data types examples

---
+++ python_basics/simple_datatypes.py
@@ -0,0 +1,25 @@
+a=10
+b=20
+
+print(a,b)
+print(type(a+b))
+# complex type
+c=3+3j
+print(type(c))
+# binary
+d=0B1010
+print(type(d))
+
+# octal
+e=0O123
+print(oct(16))
+print(bin(16))
+# bool
+
+print(type(4>3))
+
+first_name="mukesh"
+last_name="harshwal"
+ssn="1234567US"
+height=5.4
+weight=35.8
---
+++ python_basics/string_fucntions.py
@@ -0,0 +1,16 @@
+s="  mukesh harshwal  "
+# * represents repetition
+print(s*2)
+
+print("length of s is ",len(s))
+
+print(s[0:5])
+print(s[:5])
+print(s[::2])
+print(s[::-1])
+print(s[-5:-1])
+
+print(s.strip())
+print(s.lstrip())
+print(s.rstrip())
+

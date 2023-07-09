b97a4a10cbd3effeaf7c8ca1b8bb6408191717df
mukesh-harshwal <ccoredeveloper@gmail.com>
2023-07-09 23:02:56 +0530

    added list features

---
+++ python_basics/list_features.py
@@ -0,0 +1,21 @@
+l = ["mukesh",12,0b101,True]
+
+print(l)
+print(l[:len(l)])
+print(l[-3:])
+l[0]="mukesh harshwal"
+print(l)
+for e in l:
+    print(e)
+
+l.append("ashish")
+l.insert(0,"demo")
+print(l)
+del(l[2])
+print(l)
+ll=[12,34,54,56,67,87,987]
+print(max(ll))
+ll.sort()
+print(ll)
+
+print(l)
